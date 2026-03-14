26965ed67f1f0787e801b18f0b508b7442b1ac0c
TheTrueFishCake <fishcake2000yt@gmail.com>
2026-03-14 15:28:21 -0400

    Updated 3online to version 1.0
    Updated 3donline to version 1.0. Check Changelog for more info

--- 3donline/Build/v1.0.loader.js
+++ 3donline/Build/v1.0.loader.js
@@ -52,7 +52,7 @@ function createUnityInstance(canvas, config, onProgress) {
       preserveDrawingBuffer: false,
       powerPreference: 2,
     },
-    wasmFileSize: 58948780,
+    wasmFileSize: 58948557,
     streamingAssetsUrl: "StreamingAssets",
     downloadProgress: {},
     deinitializers: [],
