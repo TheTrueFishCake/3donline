24f850f79a9d07d9603bec2c76082fcd7f6a7998
TheTrueFishCake <fishcake2000yt@gmail.com>
2026-03-15 19:52:48 -0400

    Fixed lighting issues
    Fixed lighting issues with players. Changelog has been updated.

--- 3donline/Build/v1.2.loader.js
+++ 3donline/Build/v1.2.loader.js
@@ -52,7 +52,7 @@ function createUnityInstance(canvas, config, onProgress) {
       preserveDrawingBuffer: false,
       powerPreference: 2,
     },
-    wasmFileSize: 58953306,
+    wasmFileSize: 58955735,
     streamingAssetsUrl: "StreamingAssets",
     downloadProgress: {},
     deinitializers: [],
