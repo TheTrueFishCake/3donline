ec0ae29baf695795b424da966a1f712a0f673030
TheTrueFishCake <fishcake2000yt@gmail.com>
2026-03-17 09:06:36 -0400

    fixed minor issues
    please work please work please work please work please work please work please work please work please work please work please work please work please work please work please work please work please work please work please work please work please work please work please work

--- arena/test/Build/v0.1.2.loader.js
+++ arena/test/Build/v0.1.2.loader.js
@@ -52,7 +52,7 @@ function createUnityInstance(canvas, config, onProgress) {
       preserveDrawingBuffer: false,
       powerPreference: 2,
     },
-    wasmFileSize: 104792645,
+    wasmFileSize: 104796184,
     cacheControl: function (url) {
       return (url == Module.dataUrl || url.match(/\.bundle/)) ? "must-revalidate" : "no-store";
     },
